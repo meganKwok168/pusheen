2f0852b69443f85abbe08c7b5c59fe8f950f6080
Sophia <sophiac188@nycstudents.net>
2026-03-30 15:46:51 +0000

    add files for tree

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+# Pusheen
+# Softdev 2026
+# P04
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session, redirect
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

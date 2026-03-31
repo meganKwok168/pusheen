187eaa30829ebf1817903d018fd128acdcdb3dd2
Joyce Lin <joycel78@nycstudents.net>
2026-03-30 22:22:45 -0400

    got a basic flask app working

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ from flask import session, redirect
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()

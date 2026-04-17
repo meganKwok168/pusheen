7b12f3f79b21c45cd85ddbf1e0b6cca0ea2953e3
Evan Khosh <evank43@nycstudents.net>
2026-04-17 00:52:14 -0400

    Finished script to populate mongodb

--- app/mongoSetup.py
+++ app/mongoSetup.py
@@ -1,5 +1,5 @@
-pip install pymongo
-pip install kagglehub[pandas-datasets]
+#pip install pymongo
+#pip install kagglehub[pandas-datasets]
 
 from pymongo import MongoClient
 import kagglehub
@@ -7,12 +7,7 @@ from kagglehub import KaggleDatasetAdapter
 
 mongo = MongoClient("mongodb://localhost:27017")
 
-mongo.create_collection("users", {
-    account_id: <number>,
-    account_type: <string>,
-    follower_count: <number>,
-    posts: <
-})
+mongo.create_collection("users")
 mongo.create_collection("creators")
 mongo.create_collection("posts")
 
@@ -28,10 +23,43 @@ df = kagglehub.load_dataset(
   file_path,
 )
 
-docs = []
 for data in df.itertuples(index=False):
-    docs.append({
-        "": data[0],
-    })
-if docs:
-    mongo.posts.insert_many(docs)
+    mongo.creators.updateOne(
+        { "_id": data[1] },
+        {
+            "$push": { "posts": data[0] },
+            "$setOnInsert": {
+                "_id": data[1],
+                "account_type": data[2],
+                "follower_count": data[3],
+                "posts": [data[0]]
+            }
+        },
+        { upsert: true }
+    )
+
+    mongo.posts.insertOne(
+        {
+            "_id": data[0],
+            "account_id": data[1],
+            "media_type": data[4],
+            "content_category": data[5],
+            "traffic_source": data[6],
+            "has_call_to_action": data[7],
+            "likes": data[12],
+            "comments": data[13],
+            "shares": data[14],
+            "saves": data[15],
+            "reach": data[16],
+            "impressions": data[17],
+            "engagement_rate": data[18],
+            "followers_gained": data[19],
+            "caption_length": data[20],
+            "hashtags_count": data[21],
+            "performace_bucket": data[22],
+            "post_datetime": data[8],
+            "post_date": data[9],
+            "post_hour": data[10],
+            "day_of_week": data[11]
+        }
+    )
